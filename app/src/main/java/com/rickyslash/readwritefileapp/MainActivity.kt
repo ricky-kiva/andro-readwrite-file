b53e7b31607c78932a0819d5b41d90bc1b0052c0
ricky-kiva <ricky.kiva@gmail.com>
2023-03-27 12:53:34 +0700

    add MainActivity

--- app/src/main/java/com/rickyslash/readwritefileapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/readwritefileapp/MainActivity.kt
@@ -1,13 +1,75 @@
 package com.rickyslash.readwritefileapp
 
+import android.icu.text.CaseMap.Title
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.rickyslash.readwritefileapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNew.setOnClickListener(this)
+        binding.buttonOpen.setOnClickListener(this)
+        binding.buttonSave.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_new -> newFile()
+            R.id.button_open -> showList()
+            R.id.button_save -> saveFile()
+        }
+    }
+
+    private fun newFile() {
+        binding.editTitle.setText("")
+        binding.editFile.setText("")
+        Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showList() {
+        val items = fileList()
+        // create instance of 'AlertDialog'
+        val builder = AlertDialog.Builder(this)
+        // set items to be displayed in 'AlertDialog' modal
+        builder.setItems(items) { dialog, item -> loadData(items[item].toString()) }
+        // creates the 'AlertDialog'
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun saveFile() {
+        when {
+            binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "Title can't be empty", Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() -> Toast.makeText(this, "Content can't be empty", Toast.LENGTH_SHORT).show()
+            else -> {
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+                val fileModel = FileModel()
+                fileModel.filename = title
+                fileModel.data = text
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving '${fileModel.filename}' file", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loadData(title: String) {
+        val fileModel = FileHelper.readFromFile(this, title)
+        binding.editTitle.setText(fileModel.filename)
+        binding.editFile.setText(fileModel.data)
+        Toast.makeText(this, "Loading ${fileModel.filename} data", Toast.LENGTH_SHORT).show()
+    }
+
 }
 
 /* LOCAL DATA PERSISTENT */
